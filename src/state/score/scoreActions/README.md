d8dc30b2d879b17feba692d86f7b4f1d6d1acb0b
James Drummond <james@yellowmelon.co>
2021-01-24 13:50:29 -0800

    Score action documentation

---
+++ src/state/score/scoreActions/README.md
@@ -0,0 +1,7 @@
+**Score Actions**
+
+addCorrectAnswer() - Add a correct answer object to the correctAnswers list
+
+addIncorrectAnswer() - Add an incorrect answer object to the incorrectAnswers list
+
+resetAnswers() - Reset the correct and incorrect answer lists to their default state
--- src/state/score/scoreActions/scoreActions.js
+++ src/state/score/scoreActions/scoreActions.js
@@ -4,17 +4,24 @@ const SCORE_ACTIONS = {
   RESET_ANSWERS: 'RESET_ANSWERS',
 };
 
-//
+// Add a correct answer to the correct answers list
+// @param {Object} - answer. An answer or 'card' object
+// @returns {Object} - an object to be supplied as an argument to the reducer.
 const addCorrectAnswer = (answer) => ({
   type: SCORE_ACTIONS.ADD_CORRECT_ANSWER,
   answer,
 });
 
+// Add an incorrect answer to the incorrect answers list
+// @param {Object} - answer. An answer or 'card' object
+// @returns {Object} - an object to be supplied as an argument to the reducer.
 const addIncorrectAnswer = (answer) => ({
   type: SCORE_ACTIONS.ADD_INCORRECT_ANSWER,
   answer,
 });
 
+// Reset the correct and incorrect answer lists to their default state
+// @returns {Object} - an object to be supplied as an argument to the reducer.
 const resetAnswers = () => ({
   type: SCORE_ACTIONS.RESET_ANSWERS,
 });
